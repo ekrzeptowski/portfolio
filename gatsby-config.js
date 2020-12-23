34f5246a2b65d9f4da65734fb743ec8042d1b786
saj96n <saj96n@gmail.com>
2020-12-23 22:47:02 +0100

    Add gatsby webpack bundle analyser

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    `gatsby-plugin-webpack-bundle-analyser-v2`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
