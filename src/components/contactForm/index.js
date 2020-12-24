af8cc3dbc8e0d668ebd0d931647f50396775a0bc
saj96n <saj96n@gmail.com>
2020-12-24 19:15:55 +0100

    Add contact form api

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import { Input, SubmitButton, TextArea } from "../input";
 import { useForm } from "react-hook-form";
 
 import styles from "./index.module.scss";
 
+import { FaRegSmile } from "@react-icons/all-files/fa/FaRegSmile";
+import { Icon } from "../icon";
+
+function getFormData(object) {
+  const formData = new FormData();
+  Object.keys(object).forEach(key => formData.append(key, object[key]));
+  return formData;
+}
+
 const ContactForm = () => {
   const { t } = useTranslation();
 
   const { register, handleSubmit, watch } = useForm();
-  const onSubmit = data => console.log(data);
 
-  console.log(watch("name"));
+  const [sent, setSent] = useState(false);
+
+  const onSubmit = data => {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxmKCbInQ3lS5baKkXoSc3llq9JDiW4aGnN8MwPh5KbmBMPSYMy/exec",
+      {
+        method: "POST",
+        body: getFormData(data),
+      }
+    ).then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        setSent(true);
+      }
+    });
+  };
   return (
     <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
       <Input
@@ -38,6 +60,12 @@ const ContactForm = () => {
         required
       />
       <SubmitButton primary />
+      {sent && (
+        <div className={styles.overlay}>
+          <Icon icon={<FaRegSmile />} />
+          <span>{t("Message has been sent successfully")}</span>
+        </div>
+      )}
     </form>
   );
 };
