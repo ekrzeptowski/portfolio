9c9a81169dc91194326dac3fd443ac4a64aa8512
saj96n <saj96n@gmail.com>
2021-01-12 18:20:22 +0100

    Update not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,13 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+import NotFound from "../svg/not-found.svg";
 const NotFoundPage = () => (
-  <Layout>
+  <Layout style={{ paddingTop: 66 }}>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <NotFound style={{ maxWidth: 600, flexGrow: 1 }} />
+    </div>
   </Layout>
 );
 
